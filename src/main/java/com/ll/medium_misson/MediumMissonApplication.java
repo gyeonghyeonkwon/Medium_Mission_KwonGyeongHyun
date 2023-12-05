44cb56d6ca552f4c4fdcf21edf1f4d299a6e578b
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-12-05 14:32:44 +0900

    start

---
+++ src/main/java/com/ll/medium_misson/MediumMissonApplication.java
@@ -0,0 +1,13 @@
+package com.ll.medium_misson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumMissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumMissonApplication.class, args);
+    }
+
+}
