e0bfd181949d403c7abdcaf46761e190f3acba6e
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-12-05 16:00:50 +0900

    start

---
+++ src/main/java/com/ll/medium_misson/domain/home/home/contoller/HomeController.java
@@ -0,0 +1,15 @@
+package com.ll.medium_misson.domain.home.home.contoller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping("/")
+    public String showMain() {
+        return "domain/home/home/main";
+    }
+
+}
