b67982e24f5993e7055897b30b3bf4b28d261949
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-12-09 18:50:27 +0900

    답변 리포지터리 추가

---
+++ src/main/java/com/ll/medium_mission/domain/home/home/Repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ll.medium_mission.domain.home.home.Repository;
+
+import com.ll.medium_mission.domain.home.home.Entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
